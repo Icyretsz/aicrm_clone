019f9cc019e9b21ebca796daab5e1548e2206239
IcyretszMacbookPro16 <ubw1212@gmail.com>
2024-12-01 11:28:51 +0700

    Setting up

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,18 +1,17 @@
 import { NextResponse } from 'next/server';
 import { NextRequest } from 'next/server';
-import { getSession } from '@auth0/nextjs-auth0';
+import { getSession } from '@auth0/nextjs-auth0/edge';
 
-export function middleware(request: NextRequest, response: NextResponse) {
+export async function middleware(request: NextRequest, response: NextResponse) {
+  const pathname = request.nextUrl.pathname
   const host = request.headers.get('host');
-  const session = getSession(request, response);
+  const session = await getSession(request, response);
   if (host) {
     const segments = host.split('.');
     if (host.includes('localhost') ? segments.length > 1 : segments.length > 2) {
-      if (!session) {
+      if (!session && pathname !== '/main') {
         return NextResponse.redirect(
-          process.env.NODE_ENV === 'production'
-            ? `https://${host}/main`
-            : `http://${host}/main`
+          process.env.NODE_ENV === 'production' ? `https://${host}/main` : `http://${host}/main`
         );
       }
     } else {
