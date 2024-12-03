0d033bdabcb0445978dd629db875363d995a961f
IcyretszMacbookPro16 <ubw1212@gmail.com>
2024-12-03 10:08:47 +0700

    Setting up

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,17 +1,30 @@
 import { NextResponse } from 'next/server';
 import { NextRequest } from 'next/server';
 import { getSession } from '@auth0/nextjs-auth0/edge';
+import { getDomain } from '@aicrm/shared-utils';
 
 export async function middleware(request: NextRequest) {
-  const response = NextResponse.next()
-  const pathname = request.nextUrl.pathname
+  const response = NextResponse.next();
+  const pathname = request.nextUrl.pathname;
   const host = request.headers.get('host');
   const session = await getSession(request, response);
-  console.log('session', session)
+
+  console.log('session', session);
   if (host) {
+    const domain = getDomain(host);
     const segments = host.split('.');
     if (host.includes('localhost') ? segments.length > 1 : segments.length > 2) {
       if (!session && pathname !== '/main') {
+        return NextResponse.redirect(
+          process.env.NODE_ENV === 'production' ? `https://${host}/main` : `http://${host}/main`
+        );
+      } else if (session && segments[0] !== session.user.org_name) {
+        return NextResponse.redirect(
+          process.env.NODE_ENV === 'production'
+            ? `https://${domain}/error/401/unauthorized_org`
+            : `http://${domain}/error/401/unauthorized_org`
+        );
+      } else if (session) {
         return NextResponse.redirect(
           process.env.NODE_ENV === 'production' ? `https://${host}/main` : `http://${host}/main`
         );
@@ -21,6 +34,7 @@ export async function middleware(request: NextRequest) {
     }
   }
 }
+
 export const config = {
   matcher: ['/((?!_next/static|_next/image|favicon.ico|api).*)'],
 };
