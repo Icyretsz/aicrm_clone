cd31e1ee064b560c056f7d69eecac71cbc7ee08c
IcyretszMacbookPro16 <ubw1212@gmail.com>
2024-12-05 22:37:53 +0700

    setting up

--- apps/web/app/api/[...route]/hono.ts
+++ apps/web/app/api/[...route]/hono.ts
@@ -2,4 +2,6 @@ import { hc } from "hono/client"
 
 import { AppType } from '@aicrm/api';
 
-export const client = hc<AppType>('http://localhost:3002/api/')
+const baseURL = process.env.NODE_ENV === 'production' ? 'https://aicrm.club' : 'http://localhost:3002'
+
+export const client = hc<AppType>(`${baseURL}/api/`)
